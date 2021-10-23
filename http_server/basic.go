ea24ecdd01b7fdf98d571f71a4c83bd95e6ec276
Akshay Khairmode <akshaykhairmode@gmail.com>
2021-10-23 19:02:57 +0530

    updates

--- http_server/basic.go
+++ http_server/basic.go
@@ -8,20 +8,21 @@ import (
 func main() {
 
 	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
-		rw.Write([]byte("Root Path"))
+		rw.Write([]byte("Root Path\n"))
 	})
 
 	http.HandleFunc("/first", handleFirst)
 	http.HandleFunc("/second", handleSecond)
 
+	fmt.Println("Started Server")
 	fmt.Println(http.ListenAndServe(":7000", nil))
 
 }
 
 func handleFirst(rw http.ResponseWriter, r *http.Request) {
-	rw.Write([]byte("First"))
+	rw.Write([]byte("First\n"))
 }
 
 func handleSecond(rw http.ResponseWriter, r *http.Request) {
-	rw.Write([]byte("Second"))
+	rw.Write([]byte("Second\n"))
 }
