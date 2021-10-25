439451dde4dc97f57c8632eb95a2c2ecce5d355c
Akshay Khairmode <akshaykhairmode@gmail.com>
2021-10-25 19:20:14 +0530

    middleware

--- http_server/middleware.go
+++ http_server/middleware.go
@@ -4,14 +4,22 @@ import (
 	"net/http"
 )
 
+const (
+	APIKEY = "12345"
+	TOKEN  = "token"
+)
+
+type middleWare func(http.HandlerFunc) http.HandlerFunc
+
 func main() {
 
 	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
-		rw.Write([]byte("Hello World"))
+		rw.Write([]byte("Hello World\n"))
 	})
 
-	http.HandleFunc("/public", handlePublic)
-	http.HandleFunc("/private", authenticate(handlePrivate))
+	http.HandleFunc("/landing", authorize(landing))
+	http.HandleFunc("/profile", wrap(profile, authorize, authenticate))
+	http.HandleFunc("/client_api", authenticate(client))
 
 	http.ListenAndServe(":7000", nil)
 
@@ -21,8 +29,8 @@ func authenticate(next http.HandlerFunc) http.HandlerFunc {
 
 	return func(rw http.ResponseWriter, r *http.Request) {
 
-		if r.Header.Get("api-key") == "" {
-			rw.Write([]byte("Auth Error"))
+		if r.Header.Get("TOKEN") != TOKEN {
+			rw.Write([]byte("Invalid User\n"))
 			return
 		}
 
@@ -31,10 +39,38 @@ func authenticate(next http.HandlerFunc) http.HandlerFunc {
 
 }
 
-func handlePublic(rw http.ResponseWriter, r *http.Request) {
-	rw.Write([]byte("Public Endpoint Called"))
+func authorize(next http.HandlerFunc) http.HandlerFunc {
+
+	return func(rw http.ResponseWriter, r *http.Request) {
+
+		if r.Header.Get("api-key") != APIKEY {
+			rw.Write([]byte("Auth Error\n"))
+			return
+		}
+
+		next(rw, r)
+	}
+
 }
 
-func handlePrivate(rw http.ResponseWriter, r *http.Request) {
-	rw.Write([]byte("Private Endpoint Called"))
+func wrap(l http.HandlerFunc, midW ...middleWare) http.HandlerFunc {
+
+	//do it in reverse to preserve order
+	for i := len(midW) - 1; i >= 0; i-- {
+		l = midW[i](l)
+	}
+
+	return l
+}
+
+func landing(rw http.ResponseWriter, r *http.Request) {
+	rw.Write([]byte("Some Data for landing screen\n"))
+}
+
+func profile(rw http.ResponseWriter, r *http.Request) {
+	rw.Write([]byte("Some Data for profile\n"))
+}
+
+func client(rw http.ResponseWriter, r *http.Request) {
+	rw.Write([]byte("Some Data for client\n"))
 }
