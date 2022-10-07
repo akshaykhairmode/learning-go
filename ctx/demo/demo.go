27457a9d3cb63e8a4611ddd3763de160df6ccb30
Akshay Khairmode <akshaykhairmode@gmail.com>
2022-10-08 01:06:12 +0530

    update context

--- ctx/demo/demo.go
+++ ctx/demo/demo.go
@@ -6,25 +6,37 @@ type myPrivateKey string
 
 type MyExportedKey string
 
+//NOTE :: All our keys will have the same string value that is ID.
+
+//This is a normal string key, can be used anyone by outside the package
 var ctxKey = "ID"
+
+//This key is of type myPrivateKey and the type is only accessible by this package.
 var ctxPrivateKey myPrivateKey = "ID"
+
+//This key is of type MyExportedKey and the type is accessible to whoever imports this package
 var ctxExportedKey MyExportedKey = "ID"
+
+//Some value which I may not want others to change.
 var valueThatShouldNotChange = "SOME_SECRET_STRING"
 
+//returns a context which has ID key but the datatype is not exported.
 func Private() context.Context {
 	ctx := context.Background()
-	dctx := context.WithValue(ctx, "NAME", "John")
+	dctx := context.WithValue(ctx, "NAME", "John") //Default value
 	return context.WithValue(dctx, ctxPrivateKey, valueThatShouldNotChange)
 }
 
+//Returns a context which has ID key but the datatype is string that anyone can create.
 func Public() context.Context {
 	ctx := context.Background()
-	dctx := context.WithValue(ctx, "NAME", "John")
+	dctx := context.WithValue(ctx, "NAME", "John") //Default value
 	return context.WithValue(dctx, ctxKey, valueThatShouldNotChange)
 }
 
+//Returns a context which has ID key but the datatype is exported.
 func PublicWithExportedType() context.Context {
 	ctx := context.Background()
-	dctx := context.WithValue(ctx, "NAME", "John")
+	dctx := context.WithValue(ctx, "NAME", "John") //Default value
 	return context.WithValue(dctx, ctxExportedKey, valueThatShouldNotChange)
 }
--- ctx/main.go
+++ ctx/main.go
@@ -17,43 +17,66 @@ func main() {
 	ctx := context.Background() //Empty context
 	log.Println("empty context", ctx)
 
-	// WithDeadline(ctx)
-	// WithTimeout(ctx)
-	// WithCancel(ctx)
-	// WithValue(ctx)
+	WithDeadline(ctx)
+	WithTimeout(ctx)
+	WithCancel(ctx)
+	WithValue(ctx)
 
 	//With Custom context
-	realCtx, cancel := context.WithTimeout(ctx, 10*time.Second)
+	realCtx, cancel := context.WithTimeout(ctx, 20*time.Second)
 	defer cancel()
-	log.Println(someRealWorldExample(realCtx))
+	log.Println(fileReadExample(realCtx))
+
+	//With nested context
+	ctxWithTimeout, cancel := context.WithTimeout(ctx, 5*time.Second)
+	defer cancel()
+	nestedFunction(ctxWithTimeout, 0)
 
 	//With http server, caller cancelling request
 	startHttpServer()
+
+}
+
+func nestedFunction(ctx context.Context, counter int) {
+
+	for {
+		select {
+		case <-ctx.Done():
+			log.Println("returning at counter", counter)
+			return
+		default:
+			counter++
+			log.Println("Sleeping for 1 second at counter", counter)
+			time.Sleep(time.Second)
+			nestedFunction(ctx, counter)
+		}
+	}
+
 }
 
 func startHttpServer() {
 	s := http.Server{
-		Addr: ":8000",
-		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			data, err := someRealWorldExample(r.Context()) //Pass the http request context
-			if err != nil {
+		Addr: ":8000", //Start server on this port
+		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) { //On base path execute below code.
+			data, err := fileReadExample(r.Context()) //Pass the http request context
+			if err != nil {                           //Context cancels or any other erros will come here.
 				log.Println(err)
-				w.WriteHeader(500)
+				w.WriteHeader(500) //Give 500 status code if 500 error
 				w.Write([]byte(data))
 				return
 			}
 
-			w.WriteHeader(200)
+			w.WriteHeader(200) //200 status code if success
 			w.Write([]byte(data))
 			log.Println("Request Complete")
 		}),
 	}
 
 	log.Println("Starting server on port 8000")
-	log.Println(s.ListenAndServe())
+	log.Println(s.ListenAndServe()) //Start the http server
 }
 
-func someRealWorldExample(ctx context.Context) (string, error) {
+func fileReadExample(ctx context.Context) (string, error) {
 
 	f, err := os.Open("data.txt")
 	if err != nil {
@@ -61,7 +84,6 @@ func someRealWorldExample(ctx context.Context) (string, error) {
 	}
 
 	s := bufio.NewScanner(f)
-
 	out := strings.Builder{}
 
 t:
@@ -69,14 +91,19 @@ t:
 	for {
 
 		select {
+		//If context is done then we wull return from this function with error.
+		//We can choose if we want to return the data read till now or return empty string.
 		case <-ctx.Done():
 			return out.String() + "\n", fmt.Errorf("ctx cancelled")
 		default:
+			//Till my context is done default will be called to scan every line an append to our string builder.
 			if s.Scan() {
 				log.Printf("Appending %s", s.Text())
 				out.WriteString(s.Text())
 				time.Sleep(500 * time.Millisecond)
 			} else {
+				//When scan returns false, it means that the file has reached the end
+				//so we break the loop with a label.
 				break t
 			}
 		}
@@ -99,7 +126,7 @@ func WithDeadline(ctx context.Context) {
 	defer cancel() //Release the context
 
 	for {
-		select { //Random selection if both cases are ready.
+		select { //Default would be last priority here.
 		case <-ctx.Done():
 			log.Println("Exiting function WithDeadline")
 			return
@@ -119,7 +146,7 @@ func WithTimeout(ctx context.Context) {
 	defer cancel()                                         //Release the context
 
 	for {
-		select { //Random selection if both cases are ready.
+		select {
 		case <-ctx.Done():
 			log.Println("Exiting function WithTimeout")
 			return
@@ -137,13 +164,13 @@ func WithCancel(ctx context.Context) {
 	ctx, cancel := context.WithCancel(ctx) //returns a cancel func which can be called separately to send signal to done channel
 	defer cancel()                         //Release the context
 
-	go func() {
+	go func() { //in 2 seconds call the cancel function.
 		time.Sleep(2 * time.Second)
 		cancel()
 	}()
 
 	for {
-		select { //Random selection if both cases are ready.
+		select {
 		case <-ctx.Done():
 			log.Println("Exiting function WithCancel")
 			return
@@ -156,38 +183,37 @@ func WithCancel(ctx context.Context) {
 
 func WithValue(ctx context.Context) {
 
-	ctx = context.WithValue(ctx, "ID", "SOME-STRING") //returns a new context which contains the key value pair
+	//Add ID key to passed context. Replaces the old ID if existed.
+	ctx = context.WithValue(ctx, "ID", "SOME-STRING")
 
-	printContext(ctx, "ctx")
-
+	//Check comments in demo file.
 	privateCtx := demo.Private()
-
-	printContext(privateCtx, "privateCtx")
-
 	publicCtx := demo.Public()
+	exportedPublicCtx := demo.PublicWithExportedType()
 
-	printContext(publicCtx, "publicCtx")
-
-	exportedPublic := demo.PublicWithExportedType()
-
-	printContext(exportedPublic, "exportedPublic")
-
-	//Modifications below
-
+	//Trying modification on different context types.
 	modifiedPrivateCtx := context.WithValue(privateCtx, "ID", "MY-MODIFIED-STRING")
-
-	printContext(modifiedPrivateCtx, "modifiedPrivateCtx")
-
 	modifiedPublicCtx := context.WithValue(publicCtx, "ID", "MY-MODIFIED-STRING")
+	modifiedExportedCtx := context.WithValue(exportedPublicCtx, demo.MyExportedKey("ID"), "MY-MODIFIED-STRING")
 
-	printContext(modifiedPublicCtx, "modifiedPublicCtx")
+	//Maintain to print easily all of them.
+	print := map[context.Context]string{
+		ctx:                 "ctx",
+		privateCtx:          "privateCtx",
+		publicCtx:           "publicCtx",
+		exportedPublicCtx:   "exportedPublicCtx",
+		modifiedPrivateCtx:  "modifiedPrivateCtx",
+		modifiedPublicCtx:   "modifiedPublicCtx",
+		modifiedExportedCtx: "modifiedExportedCtx",
+	}
 
-	modifiedExportedCtx := context.WithValue(exportedPublic, demo.MyExportedKey("ID"), "MY-MODIFIED-STRING")
-
-	printContext(modifiedExportedCtx, "modifiedExportedCtx")
+	for ctx, what := range print {
+		printContext(ctx, what)
+	}
 
 }
 
+//Prints the context values
 func printContext(ctx context.Context, which string) {
 	log.Printf("%s ::: ID is %v, NAME is %v, exported ID is %v", which, ctx.Value("ID"), ctx.Value("NAME"), ctx.Value(demo.MyExportedKey("ID")))
 }
