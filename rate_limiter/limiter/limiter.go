acda3837d707878f4ea15862652d725ef4cb2541
Akshay Khairmode <akshaykhairmode@gmail.com>
2022-12-18 14:05:55 +0530

    add test

--- rate_limiter/limiter/limiter.go
+++ rate_limiter/limiter/limiter.go
@@ -8,13 +8,13 @@ import (
 )
 
 type status struct {
-	Limit uint64
-	Count map[string]uint64
+	Limit uint64            //The rate limit set for the endpoint
+	Count map[string]uint64 //Key will be RemoteAddr and value will be the number of current connections
 }
 
 type limiter struct {
-	det map[string]*status
-	mux sync.RWMutex
+	det map[string]*status //Key will be the endpoint here.
+	mux sync.RWMutex       //For handling concurrent read write on maps.
 }
 
 func (l *limiter) SetRateLimit(endpoint string, limit uint64) {
@@ -28,48 +28,48 @@ func (l *limiter) SetRateLimit(endpoint string, limit uint64) {
 
 func (l *limiter) incr(r *http.Request) error {
 	l.mux.Lock()
-	defer l.mux.Unlock()
+	defer l.mux.Unlock() //defering here so we dont forget later
 
 	status := l.getStatus(r)
-	if status == nil {
+	if status == nil { //if the endpoint was not initialized with set rate limit status will be nil
 		return nil
 	}
 
-	if status.Count[r.RemoteAddr] >= status.Limit {
+	if status.Count[r.RemoteAddr] >= status.Limit { //Check if the limit is exceeded.
 		return fmt.Errorf("rate limit reached for %s", r.RequestURI)
 	}
 
-	status.Count[r.RemoteAddr]++
+	status.Count[r.RemoteAddr]++ //Increase the count for this IP.
 	return nil
 }
 
 func (l *limiter) decr(r *http.Request) {
 	l.mux.Lock()
-	defer l.mux.Unlock()
+	defer l.mux.Unlock() //defering here so we dont forget later
 
 	status := l.getStatus(r)
-	if status == nil {
+	if status == nil { //if the endpoint was not initialized with set rate limit status will be nil
 		return
 	}
 
-	status.Count[r.RemoteAddr]--
+	status.Count[r.RemoteAddr]-- //Decrease the count for this IP.
 }
 
 func (l *limiter) Middleware(handler http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		if err := l.incr(r); err != nil {
+		if err := l.incr(r); err != nil { //If limit is exceeded we return too many requests
 			w.WriteHeader(http.StatusTooManyRequests)
 			w.Write([]byte(err.Error()))
 			return
 		}
-		defer l.decr(r)
-		handler.ServeHTTP(w, r)
+		defer l.decr(r)         //Decrement will happen after the actual handler is executed.
+		handler.ServeHTTP(w, r) //Actual handler
 	}
 }
 
 func (l *limiter) getStatus(r *http.Request) *status {
 
-	status, ok := l.det[r.RequestURI]
+	status, ok := l.det[r.RequestURI] //Check if the endoint exists in the map.
 
 	if !ok {
 		log.Printf("endpoint %s not found", r.RequestURI)
--- rate_limiter/limiter/limiter_test.go
+++ rate_limiter/limiter/limiter_test.go
@@ -3,38 +3,37 @@ package limiter
 import (
 	"net/http"
 	"net/http/httptest"
+	"sync/atomic"
 	"testing"
 	"time"
 )
 
 func TestLimiter(t *testing.T) {
 
-	endpointCalledCount := 0
+	endpointCalledCount := int64(0)
 
 	r := httptest.NewRequest("GET", "/test", nil)
 	w := httptest.NewRecorder()
 
-	l := NewLimiter()
-	l.SetRateLimit("/test", 5)
+	l := NewLimiter()          //Create our limiter instance
+	l.SetRateLimit("/test", 5) //set rate limit to 5 for /test endpoint.
 
-	hf := l.Middleware(func(w http.ResponseWriter, r *http.Request) {
-		endpointCalledCount++
-		time.Sleep(1 * time.Second)
-
+	hf := l.Middleware(func(w http.ResponseWriter, r *http.Request) { //This is our actual handler
+		atomic.AddInt64(&endpointCalledCount, 1) //We will increase the count in the actual handler.
+		time.Sleep(1 * time.Second)              //We are sleeping here so that we get the realistic count increase.
 	})
 
-	for i := 0; i < 200; i++ {
-		go hf(w, r)
-	}
-
 	r1 := httptest.NewRequest("GET", "/test", nil)
-	r1.RemoteAddr = "test-addr"
+	r1.RemoteAddr = "test-addr" //We create a new request here with different IP addr.
 
-	for i := 0; i < 200; i++ {
-		go hf(w, r1)
+	for i := 0; i < 200; i++ { //We start 200 go routines and call the handler with our request. Since we have rate limiting enabled for /test endpoint our actual handler should only be called 5 times.
+		go hf(w, r)
+		go hf(w, r1) //We use the same recorder as we want to increase the same count variable.
 	}
 
-	if endpointCalledCount != 10 {
+	time.Sleep(5 * time.Second) //We sleep here as we want to wait for all the go routines to finish.
+
+	if endpointCalledCount != 10 { //We are check for 10 here as our handler should get called 5 times for default remoteAddr and 5 times for test-addr
 		t.Error("endpointCalledCount does not match", endpointCalledCount)
 	}
 
